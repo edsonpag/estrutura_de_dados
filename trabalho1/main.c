92dcfa2c45ce291d2b3d973bd7c1bf179d4c8d28
edsonpag <edsonsilvapagani@gmail.com>
2020-03-30 16:47:43 -0300

    Second commit

--- trabalho1/main.c
+++ trabalho1/main.c
@@ -3,126 +3,143 @@
 #include <time.h>
 
 int main() {
+    void mostraElementosOrdenados(int *vetor, int elements, char *algoritimo);
+    void bubble(int *vetor, int elements);
+    void selection_sort(int *vetor, int elements);
+    void insertion_sort(int *vetor, int elements);
+    void chama_merge_sort(int *vetor, int elements);  
 
-    int *vetor;
+    int *vetor_1;
+    int *vetor_2;
+    int *vetor_3;
+    int *vetor_4;
     unsigned int elements = 0;
-    void bubble(int *teste, int elements);
-    void selection_sort(int *teste, int elements);
-    void insertion_sort(int *teste, int elements);
-    void merge_sort(int *teste, int inicio, int fim);
-
-
+    
     printf("Digite a quantidade de elementos: ");
     scanf("%d", &elements);
 
-    vetor = (int *)(malloc(elements * sizeof(int)));
+    vetor_1 = (int *)(malloc(elements * sizeof(int)));
+    vetor_2 = (int *)(malloc(elements * sizeof(int)));
+    vetor_3 = (int *)(malloc(elements * sizeof(int)));
+    vetor_4 = (int *)(malloc(elements * sizeof(int)));
 
-    if(vetor == NULL) {
-        printf("\nErro na alocação");
+    printf("ELEMENTOS DESORDENADOS: \n");
+
+    srand(time(NULL));
+    for(int i = 0; i < elements; i++) {
+        vetor_1[i] = rand() % 100;
+        printf("%d  ", vetor_1[i]);
+    }
+
+    printf("\n");
+
+    if(vetor_1 == NULL) {
+        printf("ERRO NA ALOCAÇÃO");
         system("pause");
         exit(1);
     }
 
-    printf("Elementos de desordenados: \n");
-
-    srand(time(NULL));
-    for(int i = 0; i < elements; i++) {
-        vetor[i] = rand() % 100;
-        printf("%d, ", vetor[i]);
-    }
-
-    // bubble(vetor, elements);
-    // selection_sort(vetor, elements);
-    // insertion_sort(vetor, elements);
-    merge_sort(vetor, 0, elements);
-
-    printf("\nElementos ordenados usando o algoritimo da merge: \n");
-
-    for(int i = 0; i < elements; i++) {
-        printf("%d, ", vetor[i]);
-    }
+    vetor_2 = vetor_1;
+    vetor_3 = vetor_1;
+    vetor_4 = vetor_1;
+        
+    bubble(vetor_1, elements);
+    selection_sort(vetor_2, elements);
+    insertion_sort(vetor_3, elements);
+    chama_merge_sort(vetor_4, elements);
 
     return 0;
 }
 
-void bubble(int *teste, int elements) {
-    int i, j, tempory;
-
-    for(i = 0; i < elements; i++) {
-        for(j = i + 1; j < elements; j++) {
-            if(teste[i] > teste[j]) {
-                tempory = teste[i];
-                teste[i] = teste[j];
-                teste[j] = tempory;
-            }
-        }
-    }
-
-    printf("\nElementos ordenados usando o algoritimo da bolha: \n");
+void mostraElementosOrdenados(int *vetor, int elements, char *algoritimo) {
+    
+    printf("ELEMENTOS ORDENADOS USANDO %s: \n", algoritimo);
 
     for(int i = 0; i < elements; i++) {
-        printf("%d, ", teste[i]);
+        printf("%d  ", vetor[i]);
     }
+    
+    printf("\n");
 }
 
-void selection_sort(int *teste, int elements) {
+void bubble(int *vetor, int elements) {
+    int j, i, aux, troca;
+
+    j = 0;
+    troca = 1;
+
+    while(j < elements && troca == 1) {
+        troca = 0;
+        for(int i = 0; i <= elements-2; i++) {
+            if(vetor[i] > vetor[i+1]) {
+                troca = 1;
+                aux = vetor[i];
+                vetor[i] = vetor[i+1];
+                vetor[i+1] = aux;
+            }
+        }
+        j++;
+    }
+    
+    mostraElementosOrdenados(vetor, elements, "BUBBLE");
+}
+
+void selection_sort(int *vetor, int elements) {
     for(int i = 0; i < elements; i++) {
         int position = i;
 
         for(int j = i + 1; j < elements; j++) {
-            if(teste[j] < teste[position]) {
+            if(vetor[j] < vetor[position]) {
                 position = j;
             }
         }
 
-        int tempory = teste[i];
-        teste[i] = teste[position];
-        teste[position] = tempory;
+        int aux = vetor[i];
+        vetor[i] = vetor[position];
+        vetor[position] = aux;
     }
 
-    printf("\nElementos ordenados com o selection sort: \n");
-
-    for(int i = 0; i < elements; i++) {
-        printf("%d, ", teste[i]);
-    }
+    mostraElementosOrdenados(vetor, elements, "SELECTION SORT");
 }
 
-void insertion_sort(int *teste, int elements) {
+void insertion_sort(int *vetor, int elements) {
     int i, j, key;
 
     for(i = 1; i < elements; i++) {
-        key = teste[i];
+        key = vetor[i];
         j = i - 1;
 
-        while(j >= 0 && key < teste[j]) {
-            teste[j+1] = teste[j];
+        while(j >= 0 && key < vetor[j]) {
+            vetor[j+1] = vetor[j];
             j--;
         }
 
-        teste[j+1] = key;
+        vetor[j+1] = key;
     }
     
-
-    printf("\nElementos ordenados com o insertion sort: \n");
-
-    for(int i = 0; i < elements; i++) {
-        printf("%d, ", teste[i]);
-    }
+    mostraElementosOrdenados(vetor, elements, "INSERTION SORT");
 }
 
-void merge_sort(int *teste, int inicio, int fim) {
+void chama_merge_sort(int *vetor, int elements) {
+    void merge_sort(int *vetor, int inicio, int fim);
+
+    merge_sort(vetor, 0, elements - 1);
+    mostraElementosOrdenados(vetor, elements, "MERGE SORT");
+}
+
+void merge_sort(int *vetor, int inicio, int fim) {
+    void intercala(int *vetor, int inicio, int fim, int meio);
     int meio;
-    void intercala(int *teste, int inicio, int fim, int meio);
 
     if(inicio < fim) {
         meio = (inicio + fim) / 2;
-        merge_sort(teste, inicio, meio);
-        merge_sort(teste, meio+1, fim);
-        intercala(teste, inicio, fim, meio);
+        merge_sort(vetor, inicio, meio);
+        merge_sort(vetor, meio+1, fim);
+        intercala(vetor, inicio, fim, meio);
     }
 }
 
-void intercala(int *teste, int inicio, int fim, int meio) {
+void intercala(int *vetor, int inicio, int fim, int meio) {
     int inicio_vetor_1, inicio_vetor_2, posicao_livre, i;
     int aux[fim];
     inicio_vetor_1 = inicio;
@@ -131,11 +148,11 @@ void intercala(int *teste, int inicio, int fim, int meio) {
 
     while(inicio_vetor_1 <= meio && inicio_vetor_2 <= fim) {
 
-        if(teste[inicio_vetor_1] <= teste[inicio_vetor_2]) {
-            aux[posicao_livre] = teste[inicio_vetor_1];
+        if(vetor[inicio_vetor_1] <= vetor[inicio_vetor_2]) {
+            aux[posicao_livre] = vetor[inicio_vetor_1];
             inicio_vetor_1++;
         } else {
-            aux[posicao_livre] = teste[inicio_vetor_2];
+            aux[posicao_livre] = vetor[inicio_vetor_2];
             inicio_vetor_2++;
         }
 
@@ -143,16 +160,16 @@ void intercala(int *teste, int inicio, int fim, int meio) {
     }
 
     for(i = inicio_vetor_1; i <= meio; i++) {
-        aux[posicao_livre] = teste[i];
+        aux[posicao_livre] = vetor[i];
         posicao_livre++;
     }
 
     for(i = inicio_vetor_2; i <= fim; i++) {
-        aux[posicao_livre] = teste[i];
+        aux[posicao_livre] = vetor[i];
         posicao_livre++;
     }
 
     for(i = inicio; i <= fim; i++) {
-        teste[i] = aux[i];
+        vetor[i] = aux[i];
     }
 }
