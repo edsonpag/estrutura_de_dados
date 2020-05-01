1d2373851e96d433c27e6c8a004e43870a7d9177
edsonpag <edsonsilvapagani@gmail.com>
2020-05-01 16:35:35 -0300

    Fixed bugs

--- TR4-algoritimo_de_busca_string/main.c
+++ TR4-algoritimo_de_busca_string/main.c
@@ -1,51 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 #define ASCII 256
-#define NUMBER_PRIME 3
+#define NUMBER_PRIME 101
 
-char str[1000000];
+char fullText[5000];
 
-void hash(char *word, char *file, int hash_word) {
+void search(char *word, int world_hash, char *file) {
+    int file_hash = 0;
+    const int word_length = strlen(word);
+    const int file_length = strlen(file);
+    int aux = 1;
 
-    int hash_file = 0;
-    int i = 0;
-
-    while(hash_file != -1) {
-
-        hash_file = pow(ASCII, hash_file) + word[i];
-
-        if(hash_file == hash_word) {
-            printf("Ok");
-            exit(0);
-        }
-
-        if(hash_file > hash_word) {
-            hash_file = 0;
-        }
-
-        i++;
+    for(int i = 0; i < word_length - 1; i++) {
+        aux = (aux * ASCII) % NUMBER_PRIME;
     }
-    
-}
-
-void rabin_karp(char *word, char *file) {
-    const int word_length = strlen(word) - 1;
-    //const int file_length = strlen(file);
-
-    int hash_word = 0;
 
     for(int i = 0; i < word_length; i++) {
-        hash_word = pow(ASCII, hash_word) + word[i];
+        file_hash = (ASCII * file_hash + file[i]) % NUMBER_PRIME;
     }
 
-    hash(word, file, hash_word);
+    for(int i = 0; i <= file_length - word_length; i++) {
+
+        if(file_hash == world_hash) {
+            int j;
+
+            for(j = 0; j < word_length; j++) {
+
+                if(file[i+j] != word[j]) {
+                    j = word_length + 1;
+                }
+
+            }
+
+            if(j == word_length) {
+                printf("A palavra/frase \"%s\" foi encontrada\n", word);
+                printf("Ela inicia no caractere %i e termina no caractere %i\n", i+1, i + word_length);
+                exit(0);
+            }
+ 
+        }
+
+        if(i < file_length - word_length) {
+            file_hash = (ASCII * (file_hash - file[i] * aux) + file[i + word_length]) % NUMBER_PRIME;
+        }
+
+        if(file_hash < 0) {
+            file_hash = (file_hash + NUMBER_PRIME);
+        }
+    }
+
+}
+
+int hashWorld(char *word) {
+    int world_hash = 0;
+    const int word_length = strlen(word);
+
+    for(int i = 0; i <  word_length; i++) {
+        world_hash = (ASCII * world_hash + word[i]) % NUMBER_PRIME;
+    }
+
+    return world_hash;
+}
+
+void rabin_karp(char *word) {
+    const int world_hash = hashWorld(word);
+
+    search(word, world_hash, fullText);
 }
 
 
-int naive(char *word, char *file) {
-    const int word_length = strlen(word) - 1; // -1 para tirar o caractere do enter
+void naive(char *word, char *file) {
+    const int word_length = strlen(word);
     const int file_length = strlen(file);
 
     for(int i = 0; i <= file_length - word_length; i++) {
@@ -57,44 +83,54 @@ int naive(char *word, char *file) {
         }
 
         if(auxiliar == word_length) {
-            return 1;
+            printf("A palavra/frase \"%s\" foi encontrada\n", word);
+            printf("Ela inicia no caractere %i e termina no caractere %i\n", i + 1, i + auxiliar);
         }
     }
+}
 
-    return 0;
+char * formattingTheWord(char word[]) {
+    int word_length = strlen(word);
+    if(word[word_length - 1] == '\n') {
+        word[word_length - 1] = '\0';
+    }
+
+    return word;
 }
 
 void read_file() {
-    char auxiliar[100];
-    
-    FILE *file;
-    file = fopen("arquivo.txt", "r");
-    
-    while(fgets(auxiliar, 100, file) != NULL) {
-        strcat(str, auxiliar);
+    char text_line[20];
+    FILE *file = fopen("arquivo.txt", "r");
+
+    if(file == NULL) {
+        printf("Erro na leitura");
+    }
+
+    while(fgets(text_line, 20, file) != NULL) {
+
+        for(int i = 0; i < 20 - 1; i++) {
+
+            if(text_line[i] == '\n') {
+                text_line[i] = '\0';
+            }
+        }
+
+        strcat(fullText, text_line);
     }
 }
 
 int main() {
-    const int word_length = 50; 
-    char word[word_length];
-    int verify;
+    char *word;
+    char aux[50];
 
     read_file();
 
-
     printf("Digite uma frase para pesquisa:\n");
-    fgets(word, word_length, stdin);
-    
-    /*
-    verify = naive(word, str);
+    fgets(aux, 50, stdin);
 
-    if(verify == 1) {
-        printf("Encontramos a palavra/frase: %s\n", word);
-    } else {
-        printf("Não encontramos a palavra\n");
-    }
-    */
+    word = formattingTheWord(aux);
 
-    rabin_karp(word, str);
+    //naive(word, fullText);
+
+    rabin_karp(word);
 }
