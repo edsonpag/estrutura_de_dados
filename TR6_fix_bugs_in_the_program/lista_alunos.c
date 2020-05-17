2fcf7b78477f47656858a7d520411b785b743848
edsonpag <edsonsilvapagani@gmail.com>
2020-05-17 09:39:06 -0300

    Correção do bug que não mostrava o ultimo aluno da lista

--- TR6_fix_bugs_in_the_program/lista_alunos.c
+++ TR6_fix_bugs_in_the_program/lista_alunos.c
@@ -1,5 +1,3 @@
-// http://hpca23.cse.tamu.edu/taco/utsa-www/ut/utsa/cs1723/lecture4.html
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,82 +6,73 @@ typedef struct _student {
 	int	id;
 	char name[100];
 	float gpa;
-} student;
+} Student;
 
-
-// nó
 typedef struct _node {
-	student	k;
-	struct _node *next; // ponteiro para o próximo elemento
-} node, *list;
+	Student	current;
+	struct _node *next;
+} Node, *List;
 
 
-void insert_list (list *L, student k) {
-	node *p;
-	p = (node *) malloc (sizeof (node));
-	p->k = k;
+void insert_list (List *L, Student k) {
+	Node *p;
+	p = (Node *) malloc (sizeof (Node));
+	p->current = k;
 	p->next = *L;
 
 	*L = p;
 }
 
 
-student *search_list (list L, int id) {
-	node *p;
+Student *search_list (List student_list, int id) {
+	Node *p;
 
-	for (p=L; p && p->k.id != id; p=p->next);
+	for (p=student_list; p && p->current.id != id; p=p->next);
 
 	if (p)
-		return &p->k;
+		return &p->current;
 	else
 		return NULL;
 }
 
-/* make an empty list */
-
-void create_list (list *L) {
+void create_list (List *L) {
 	*L = NULL;
 }
 
 
 int main () {
-	list C;	/* a class of students */
-	student s, *p;
+	List student_list;
+	Student student, *found_student;
 	int	id;
-	FILE *f;
+	FILE *file;
 
 
-
-	f = fopen ("./List.txt", "r");
-	if (!f) {
+	file = fopen ("./List.txt", "r");
+	if (!file) {
 		perror ("List");
 		exit (1);
 	}
 
-	create_list (&C);
+	create_list (&student_list);
 
-	for (;;) {
-		fscanf (f, "%d %s %f\n", &s.id, s.name, &s.gpa);
-		if (feof (f))
-      		break;
-		insert_list (&C, s);
-	}
-	fclose (f);
+	while(!feof(file)) {
+		fscanf(file, "%d %s %f\n", &student.id, student.name, &student.gpa);
+		insert_list(&student_list, student);
+	}	
 
-	for (;;) {
+	fclose (file);
+
+	while(id != -1) {
 		printf ("Enter student ID, -1 to finish: ");
 		scanf ("%d", &id);
 
-		if (id == -1)
-       		break;
+		found_student = search_list (student_list, id);
 
-		p = search_list (C, id);
-
-
-		if (!p)
+		if (!found_student)
 			printf ("ID #%d not found!\n", id);
 		else
-			printf ("%d\t%s\t%0.2f\n", p->id, p->name, p->gpa);
+			printf ("%d\t%s\t%0.2f\n", found_student->id, found_student->name, found_student->gpa);
 	}
+
 	exit (0);
 }
