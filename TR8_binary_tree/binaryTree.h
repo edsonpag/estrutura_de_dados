db2739af1246cd2a66f92d7efcb3f622b609c601
edsonpag <edsonsilvapagani@gmail.com>
2020-06-16 09:54:05 -0300

    initial functions

--- TR8_binary_tree/binaryTree.h
+++ TR8_binary_tree/binaryTree.h
@@ -7,3 +7,6 @@ struct Tree {
 struct Tree * createNode(int data);
 void insert(struct Tree **tree, int data);
 void insertNode(struct Tree **tree, int data);
+int search(struct Tree *tree, int data);
+void total(struct Tree *tree, int *results);
+void numberOfNodes(struct Tree *tree, int *nodes);
